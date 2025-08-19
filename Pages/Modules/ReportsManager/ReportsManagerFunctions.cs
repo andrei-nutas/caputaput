55e3e4a93fe204398f32729505fb071e594eab9d
andrei-nutas <andrei.nutas@gmail.com>
2025-08-20 00:49:58 +0300

    Add files via upload

---
+++ Pages/Modules/ReportsManager/ReportsManagerFunctions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Playwright;
+using TAF_iSAMS.Pages.Utilities;
+using Ui = TAF_iSAMS.Pages.Modules.ReportsManager.ReportsManagerUIMap;
+
+namespace TAF_iSAMS.Pages.Modules.ReportsManager
+{
+    internal class ReportsManagerFunctions : ModuleBasePage
+    {
+
+        private ReportsManagerFunctions(IPage page) : base(page) { }
+
+        /// <summary>
+        /// Factory method to create and initialize the School Manager page.
+        /// </summary>
+        /// 
+        #region Setup
+        public static async Task<ReportsManagerFunctions> CreateAsync(IPage page)
+        {
+            Console.WriteLine($"Creating ReportsManagerFunctions instance.");
+            var instance = new ReportsManagerFunctions(page);
+            // Initialize the iframe hierarchy for the Reports Manager module (selector for module container).
+            await instance.InitializeFramesAsync(Ui.ModuleContentLocator, Ui.ModuleContentFrame);
+            Console.WriteLine($"ReportsManagerFunctions instance created and initialized.");
+            return instance;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Pages/Modules/ReportsManager/ReportsManagerUIMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAF_iSAMS.Pages.Utilities;
+
+namespace TAF_iSAMS.Pages.Modules.ReportsManager
+{
+    internal class ReportsManagerUIMap
+    {
+        public const string AddReportsManagerButton = "#addReportsManagerButton";
+        public static string ModuleContentLocator = "#module-content-" + ModuleSelectorHelper.GetModuleNr("Reports Manager");
+        public static string ModuleContentFrame = "iFrameContent";
+        public static string ModuleOptionsFrame = "iFrameOptions";
+    }
+}
