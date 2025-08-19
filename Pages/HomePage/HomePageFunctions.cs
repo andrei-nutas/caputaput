bf6262cbebbab0342009bfbe5e3b4aa4df30849c
andrei-nutas <andrei.nutas@gmail.com>
2025-08-20 00:44:11 +0300

    Add files via upload

---
+++ Pages/HomePage/HomePageFunctions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Playwright;
+using TAF_iSAMS.Pages.Utilities;
+using Ui = TAF_iSAMS.Pages.HomePage.HomePageUiMap;
+
+namespace TAF_iSAMS.Pages.HomePage
+{
+    public class HomePageFunctions : BasePage
+    {
+        public HomePageFunctions(IPage page) : base(page) { }
+
+        public async Task ClickViewAllModulesAsync()
+        {
+            Console.WriteLine($"Clicking on View All Modules button.");
+            await Page.Locator(Ui.ViewAllModulesButton).ClickAsync();
+            Console.WriteLine($"Clicked on View All Modules button.");
+        }
+
+        public async Task SearchAndClickModuleAsync(string moduleName)
+        {
+            Console.WriteLine($"Searching for module: {moduleName}");
+
+            // Fill the module search input.
+            await Page.Locator(Ui.ModuleSearchInput).FillAsync(moduleName);
+            Console.WriteLine($"Filled the module search with: {moduleName}");
+
+            // Retrieve the selector using the helper function.
+            string moduleSelector = $"a[id='{ModuleSelectorHelper.GetModuleNr(moduleName)}']";
+            Console.WriteLine($"Retrieved selector: {moduleSelector} for module: {moduleName}");
+
+            // Click on the module using the retrieved selector.
+            await Page.Locator(moduleSelector).ClickAsync();
+            Console.WriteLine($"Clicked on the module using selector: {moduleSelector}");
+        }
+    }
+}
---
+++ Pages/HomePage/HomePageUiMap.cs
@@ -0,0 +1,9 @@
+﻿namespace TAF_iSAMS.Pages.HomePage
+{
+    public static class HomePageUiMap
+    {
+        public const string ViewAllModulesButton = "[title='View All Modules']";
+        public const string ModuleSearchInput = "#sidebar-searchbox";
+        //Note that this selector needs to be configurable
+    }
+}
