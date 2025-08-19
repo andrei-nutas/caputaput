586806f465f5182feeb74309462b21e7ff59ec78
andrei-nutas <andrei.nutas@gmail.com>
2025-08-20 00:47:40 +0300

    Add files via upload

---
+++ Pages/Modules/CensusManager/CensusManagerFunctions.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAF_iSAMS.Pages.Modules.AdmissionsManager;
+using TAF_iSAMS.Pages.Utilities;
+using Ui = TAF_iSAMS.Pages.Modules.CensusManager.CensusManagerUIMap;
+
+namespace TAF_iSAMS.Pages.Modules.CensusManager
+{
+    class CensusManagerFunctions : ModuleBasePage
+    {
+
+        // Private constructor to enforce use of the factory method.
+        private CensusManagerFunctions(IPage page) : base(page) { }
+
+        /// <summary>
+        /// Factory method to create and initialize the Census Manager page.
+        /// </summary>
+        /// 
+
+        public static async Task<CensusManagerFunctions> CreateAsync(IPage page)
+        {
+            Console.WriteLine($"Creating CensusManagerFunctions instance.");
+            var instance = new CensusManagerFunctions(page);
+            // Initialize the iframe hierarchy for the Census Manager module (selector for module container).
+            await instance.InitializeFramesAsync(Ui.ModuleContentLocator);
+            Console.WriteLine($"CensusManagerFunctions instance created and initialized.");
+            return instance;
+        }
+
+        public SQLConnector connector = new SQLConnector();
+
+        #region Configuration > Manage Census Settings
+        public async Task SchoolTypeNonIndependent()=>
+            await SelectDropDownElement(Ui.SchoolTypeDropDown, Ui.SchoolTypeNonIndependent, NavigatorTypes.Types.NestedContent);
+        public async Task SchoolTypeIndependent() =>
+           await SelectDropDownElement(Ui.SchoolTypeDropDown, Ui.SchoolTypeIndependent, NavigatorTypes.Types.NestedContent);
+        #endregion
+
+        #region Build Tab
+        public async Task StartISCCensus() =>
+            await ClickRowElement(Ui.StartISCCensusRowText, Ui.StartCensusButton, NavigatorTypes.Types.NestedContent);
+        public async Task StartSchoolLevelAnnualCensus() =>
+            await ClickRowElement(Ui.StartSchoolLevelAnnualCensusRowText, Ui.StartCensusButton, NavigatorTypes.Types.NestedContent);
+        public async Task StartSchoolCensus() =>
+            await ClickRowElement(Ui.StartSchoolCensusRowText, Ui.StartCensusButton, NavigatorTypes.Types.NestedContent);
+        public async Task StartSchoolWorkforceCensus() =>
+            await ClickRowElement(Ui.StartWorkforceCensusRowText, Ui.StartCensusButton, NavigatorTypes.Types.NestedContent);
+        #endregion
+
+        #region Census
+        public async Task ValidateCensus() =>
+            await ClickElement(Ui.ValidateCensusButton, NavigatorTypes.Types.NestedContent);
+
+        public async Task<IElementHandle> GetViewReportButton() =>
+            await GetElement(Ui.ViewAndDownloadCensusReportButton, NavigatorTypes.Types.NestedContent);
+
+        public async Task<bool> ViewReportButtonVisible()=>       
+            await IsElementVisible(Ui.ViewAndDownloadCensusReportButton, NavigatorTypes.Types.NestedContent);
+        public async Task<bool> ViewSummaryButtonVisible() =>
+            await IsElementVisible(Ui.ViewSummaryReportAndDownloadCensusButton, NavigatorTypes.Types.NestedContent);
+        public async Task<bool> WaitForBoardingHouseToBeVisible() =>
+            await WaitForElementToBeVisible(Ui.BoardingHouseIndicator, NavigatorTypes.Types.NestedContent);
+        #endregion
+
+        #region SQL Commands
+        public void SQLSetSchoolTypeIndependent()=>       
+            connector.ExecuteQuery(Ui.SQLSchoolTypeIndependent);
+        public void SQLSetSchoolTypeNonIndependent() =>
+           connector.ExecuteQuery(Ui.SQLSchoolTypeNonIndependent);
+        #endregion
+
+    }
+}
---
+++ Pages/Modules/CensusManager/CensusManagerUIMap.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAF_iSAMS.Pages.Utilities;
+
+namespace TAF_iSAMS.Pages.Modules.CensusManager
+{
+    class CensusManagerUIMap
+    {
+        #region Census Manager
+        public const string AddAdmissionButton = "#addCensusButton";
+        public static string ModuleContentLocator = "#module-content-" + ModuleSelectorHelper.GetModuleNr("Census Manager");
+        #endregion
+
+        #region Configuration > Manage Census Settings
+        public static string SchoolTypeDropDown = "select[id=lbSchoolType]";
+        public static string SchoolTypeIndependent = "Independent School";
+        public static string SchoolTypeNonIndependent = "Non-Independent School";
+        public static string SaveSettingsButtons = "div:has-text('Save Settings')";
+        #endregion
+
+        #region Build Tab
+        public static string StartISCCensusRowText = "ISC Annual Census 2025";
+        public static string StartSchoolLevelAnnualCensusRowText = "School Level Annual School Census 2025";
+        public static string StartSchoolCensusRowText = "School Census 2024/25"; //using part of the name because sufix may vary
+        public static string StartWorkforceCensusRowText = "School Workforce Census 2024";
+        public static string StartCensusButton = "Start Census";
+        #endregion
+
+        #region Census 
+        public static string ValidateCensusButton = "span:has-text('Validate Census')";
+        public static string ViewAndDownloadCensusReportButton = "span:has-text('View Report and Download Census')";
+        public static string ViewSummaryReportAndDownloadCensusButton = "span:has-text('View Summary Report and Download Census')";
+        public static string BoardingHouseIndicator = "text='Boarding House'";
+        #endregion
+
+        #region SQL Commands
+        public const string SQLSchoolTypeIndependent = "update TblCensusManagerSchoolCensusConfiguration SET intSchoolType = 1";
+        public const string SQLSchoolTypeNonIndependent = "update TblCensusManagerSchoolCensusConfiguration SET intSchoolType = 2";
+        #endregion
+
+        //copemtency matrix for automation engineer//
+    }
+}
